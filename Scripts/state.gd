c42a2e42c1849eecd110a6203dbc1833a9027f0b
IllinaBurlace <chloeburlace@gmail.com>
2025-09-29 10:29:13 +0100

    Create State abstract

---
+++ Scripts/state.gd
@@ -0,0 +1,18 @@
+@abstract
+class_name State
+extends Node
+
+func process_frame(_delta: float) -> void:
+	pass
+
+func process_phys(_delta: float) -> void:
+	pass
+
+func process_state(_delta: float) -> State:
+	return null
+
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
