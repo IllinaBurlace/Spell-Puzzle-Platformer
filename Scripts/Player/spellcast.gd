105aa8f85a39a9fd06ef08cfdcd9f922fe131d7a
IllinaBurlace <chloeburlace@gmail.com>
2025-10-02 19:25:13 +0100

    Attempted (and failed) fix of simplify function

--- Scripts/Player/spellcast.gd
+++ Scripts/Player/spellcast.gd
@@ -21,31 +21,31 @@ func _process(delta: float) -> void:
 func cast() -> void:
 	print("Cast!")
 	var simple = simplify(line.get_points())
+	print(line.points)
 	print(simple)
 	line.clear_points()
 
 func simplify(input: PackedVector2Array) -> PackedVector2Array:
-	var ret: PackedVector2Array = []
-
 	var curve: Curve2D = Curve2D.new()
 	curve.add_point(input[0])
-	curve.add_point(input[input.size() - 1])
+	curve.add_point(input[-1])
 	
-	var highest: Dictionary = {"id": 0, "dist": 0}
+	var highest: Dictionary = {"id": 0, "dist": 0.0}
 	var current: int = 0
 	for point in input:
-		var offset: float = curve.get_closest_offset(point)
-		var pos: Vector2 = curve.sample_baked(offset)
-		if pos.distance_to(point) > highest.dist:
-			highest.id = current
-			highest.dist = pos.distance_to(point)
-
+		if current > 0:
+			var offset: float = curve.get_closest_offset(point)
+			var pos: Vector2 = curve.sample_baked(offset)
+			if pos.distance_to(point) > highest["dist"]:
+				highest["id"] = current
+				highest["dist"] = pos.distance_to(point)
 		current += 1
 	
-	if highest.dist > epsilon:
-		ret = simplify(input.slice(0, highest.id + 1))
-		ret.append_array(simplify(input.slice(highest.id, input.size())))
+	var ret: PackedVector2Array = []
+	if highest["dist"] > epsilon:
+		ret = simplify(input.slice(0, highest["id"] + 1))
+		ret.append_array(simplify(input.slice(highest["id"] + 1, -1)))
 	else:
-		ret = curve.get_baked_points()
+		ret = [input[0], input[-1]]
 
 	return ret
