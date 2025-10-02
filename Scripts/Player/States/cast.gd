754e84d5072763edf4cf081d5baaa30990438733
IllinaBurlace <chloeburlace@gmail.com>
2025-10-02 18:30:51 +0100

    [[INCOMPLETE; FIX SIMPLIFY FUNCTION AND ADD EFFECTS!]] Added spellcasting

--- Scripts/Player/States/cast.gd
+++ Scripts/Player/States/cast.gd
@@ -1,8 +1,20 @@
 class_name PlayerCastState
 extends State
 
+var spell: SpellCast
+
+func _ready() -> void:
+	super()
+	spell = player.get_node("Spellcast")
+
 func enter() -> void:
 	player.sprite.modulate = Color8(0, 0, 255)
+	spell.visible = true
+	spell.line.add_point(get_viewport().get_mouse_position())
+
+func exit() -> void:
+	spell.visible = false
+	spell.cast()
 
 func process_state() -> State:
 	if Input.is_action_just_pressed("cast"):
---
+++ Scripts/Player/spellcast.gd
@@ -0,0 +1,51 @@
+class_name SpellCast
+extends Control
+
+@export
+var epsilon: float = 30.0
+@onready
+var player: Player = get_parent()
+@onready
+var mouse_tracker: Node2D = $MouseTracker
+@onready 
+var line: Line2D = $Glyph
+
+func _process(delta: float) -> void:
+	global_position = Vector2.ZERO
+
+	if visible:
+		mouse_tracker.global_position = get_viewport().get_mouse_position()
+
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			line.add_point(mouse_tracker.global_position)
+func cast() -> void:
+	print("Cast!")
+	var simple = simplify(line.get_points())
+	print(simple)
+	line.clear_points()
+
+func simplify(input: PackedVector2Array) -> PackedVector2Array:
+	var ret: PackedVector2Array = []
+
+	var curve: Curve2D = Curve2D.new()
+	curve.add_point(input[0])
+	curve.add_point(input[input.size() - 1])
+	
+	var highest: Dictionary = {"id": 0, "dist": 0}
+	var current: int = 0
+	for point in input:
+		var offset: float = curve.get_closest_offset(point)
+		var pos: Vector2 = curve.sample_baked(offset)
+		if pos.distance_to(point) > highest.dist:
+			highest.id = current
+			highest.dist = pos.distance_to(point)
+
+		current += 1
+	
+	if highest.dist > epsilon:
+		ret = simplify(input.slice(0, highest.id + 1))
+		ret.append_array(simplify(input.slice(highest.id, input.size())))
+	else:
+		ret = curve.get_baked_points()
+
+	return ret
